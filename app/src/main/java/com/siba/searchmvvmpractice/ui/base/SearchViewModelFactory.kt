52d84ba93c123a7a540d7e4c333f19a906c29848
SSong-develop <thdgnsrl123@naver.com>
2020-11-23 15:52:41 +0900

    update advice

--- app/src/main/java/com/siba/searchmvvmpractice/ui/base/SearchViewModelFactory.kt
+++ app/src/main/java/com/siba/searchmvvmpractice/ui/base/SearchViewModelFactory.kt
@@ -5,13 +5,18 @@ import androidx.lifecycle.ViewModelProvider
 import com.siba.searchmvvmpractice.repository.SearchRepository
 import com.siba.searchmvvmpractice.ui.viewmodel.SearchViewModel
 
+@Suppress("UNCHECKED_CAST")
 class SearchViewModelFactory(private val repository: SearchRepository) :
     ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
+    /*    if (modelClass.isAssignableFrom(SearchViewModel::class.java)) {
             return SearchViewModel(repository) as T
         }
-        throw IllegalArgumentException("Unknown class name")
+        throw IllegalArgumentException("Unknown class name")*/
+        
+        // SearchViewModel(repository) as T에서 T는 ViewModel 로 타입이 일치한다. 하지만 에러가 나타나 suppress 해주었다.
+        require(modelClass.isAssignableFrom(SearchViewModel::class.java)){"Unknown class name"}
+        return SearchViewModel(repository) as T
     }
 
 }
