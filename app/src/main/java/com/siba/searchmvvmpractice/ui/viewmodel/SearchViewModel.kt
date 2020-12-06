846a3ef7f121be3e2e0d483cfbcee1345ff704bc
SSong-develop <thdgnsrl123@naver.com>
2020-12-06 18:51:59 +0900

    feat: Day03. 주석 삭제

--- app/src/main/java/com/siba/searchmvvmpractice/ui/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/siba/searchmvvmpractice/ui/viewmodel/SearchViewModel.kt
@@ -13,41 +13,32 @@ import kotlinx.coroutines.launch
 class SearchViewModel(
     private val repository: SearchRepository
 ) : ViewModel() {
+
     var networkChecked: Boolean = false
 
     private val _keyword = MutableLiveData<String>()
     val keyword: MutableLiveData<String>
         get() = _keyword
 
-    // RecentSearchTerm
     var allRecentSearchTerm: LiveData<List<RecentSearchTerm>> = repository.getAllSearchTerm()
-
 
     fun insertRecentSearchTermToAppDatabase() = viewModelScope.launch {
         val recentSearchTerm = RecentSearchTerm(keyword = _keyword.value.toString())
         repository.insertRecentSearchTerm(recentSearchTerm)
     }
 
-    // GithubUser
-
-    // this function must use Network connect state is on
     fun insertGithubUserToAppDatabase() = viewModelScope.launch {
         repository.insertGithubUserToAppDatabase(_keyword.value.toString())
     }
 
-    // this function use Network connect state is off
     fun fetchGithubUserFromAppDatabase(keyword: String): LiveData<List<DomainUsers>> {
         return repository.fetchDatabaseGithubUser(keyword)
     }
 
-    // GithubRepository
-
-    // this function must use Network connect state is on
     fun insertGithubRepositoryToAppDatabase() = viewModelScope.launch {
         repository.insertGithubRepositoryToAppDatabase(_keyword.value.toString())
     }
 
-    // this function use Network connect state is off
     fun fetchGithubRepositoryFromAppDatabase(keyword: String): LiveData<List<DomainRepository>> {
         return repository.fetchDatabaseGithubRepository(keyword)
     }
